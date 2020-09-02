6045984dedb384abb61686919d90868ea2209bac
beco991 <53886609+beco991@users.noreply.github.com>
2020-09-03 00:25:03 +0200

    Add files via upload

---
+++ Administracija.ViewModel/LoginWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Administracija.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administracija.ViewModel
+{
+    public class LoginWindowViewModel : INotifyPropertyChanged
+    {
+
+        private User currentUser;
+        public User CurrentUser
+        {
+            get { return CurrentUser; }
+            set
+            {
+                if (CurrentUser == value)
+                {
+                    return;
+                }
+                CurrentUser = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("CurrentUser"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+    }
+}
---
+++ Administracija.ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,71 @@
+﻿using Administracija.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administracija.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        #region Polja
+
+        private User currentUser;
+        private UserCollection userList;
+
+        #endregion
+
+        #region Propertiji
+
+        public User CurrentUser
+        {
+            get { return currentUser; }
+            set
+            {
+                if (currentUser==value)
+                {
+                    return;
+                }
+                currentUser = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("CurrentUser"));
+            }
+        }
+
+        public UserCollection UserList
+        {
+            get { return userList; }
+            set
+            {
+                if(userList == value)
+                {
+                    return;
+                }
+                userList = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("UserList"));
+            }
+        }
+
+        #endregion
+
+        #region Konstruktori
+
+        public MainWindowViewModel()
+        {
+            UserList = UserCollection.GetAllUsers();
+            CurrentUser = new User();
+        }
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, e);
+        }
+
+    }
+}
